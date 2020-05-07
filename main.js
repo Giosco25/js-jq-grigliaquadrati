17341cd312ead81e4b9030964214b0293ccef118
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-05-07 17:44:47 +0200

    Fine esercizio con Bonus 2

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 // Esercitazione di oggi: Quadratini
 // Creare in HTML una griglia di 5x5 quadrati vuoti.
 // Con jQuery, scrivere un numero random tra 0 e 10 dentro ognuno.
-var numeri_random = genera_random(0,11)
-console.log(numeri_random);
+
 // Genero numeri random da 1 a 10
 function genera_random(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 $('.quadratini').each(function(){
-    var numeri_random = genera_random(0,11)
+    var numeri_random = genera_random(0,11);
+    console.log(numeri_random);
     $(this).append(numeri_random);
 
+// Bonus 2 se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+
+    if(numeri_random == 0){
+    // se il numero è 0 sarà di colore verde
+         $(this).addClass('zero')
+    } else if (numeri_random % 2 == 0) {
+    // se il numero è pari sarà di colore rosso
+        $(this).addClass('pari-rosso')
+    } else {
+        // se il numero è dispari sarà di colore nero
+        $(this).addClass('dispari-nero')
+    }
+
 })
+
+
+// BONUS 1
+// for (var i = 0; i < 25; i++) {
+//  $('#griglia').append('<div class="quadratini"></div>')
+// }
